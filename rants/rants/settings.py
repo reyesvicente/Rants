322955cef8ef71fa49f6176a5bc8af36764fdabf
Vicente Antonio G. Reyes <icenreyes@icloud.com>
2023-04-13 16:32:28 +0800

    Revert "added django-extensions, crispy-bootstrap5 and allauth templates"
    This reverts commit 500be01d87190613e1ab07ebf994e7a832f01f4f.

--- rants/rants/settings.py
+++ rants/rants/settings.py
@@ -44,9 +44,6 @@ INSTALLED_APPS = [
     'allauth', 
     'allauth.account',
     'allauth.socialaccount',
-    'django_extensions',
-    "crispy_forms",
-    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -180,7 +177,3 @@ if DEBUG:
     ]
 
     INSTALLED_APPS += ["debug_toolbar",]
-
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-
-CRISPY_TEMPLATE_PACK = "bootstrap5"
