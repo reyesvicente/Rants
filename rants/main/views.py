3ecae4201df351c5c9d59b8865bdd67f63f4c2d6
Vicente Antonio G. Reyes <icenreyes@icloud.com>
2023-04-13 15:01:22 +0800

    home page view 4 rants

--- rants/main/views.py
+++ rants/main/views.py
@@ -3,11 +3,16 @@ from django.views.generic.detail import DetailView
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Rant, Category
+from .models import Rant
 
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_rant'] = Rant.objects.all()[:4]
+        return context
 
 
 class RantListView(ListView):
